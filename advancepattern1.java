ccaf27e6f7b42c7cb9b7d0a2e27975299e55f2f4
unknown <you@example.com>
2026-01-20 23:51:11 +0530

    advance pattern

---
+++ advancepattern1.java
@@ -0,0 +1,6 @@
+// Your First Java Program
+public class advancepattern1 {
+   public static void main(String[] args) {
+       System.out.println("Hello, World!"); // Prints "Hello, World!" to the console
+   }
+}
